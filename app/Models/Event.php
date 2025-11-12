6562063c9dcd647fb4090f2fda9302c84756be9c
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-11-12 10:34:50 -0300

    feat: create a model event and a migration

---
+++ app/Models/Event.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+   protected $fillable = [
+        'user_id',
+        'full_name',
+        'whatsapp',
+        'all_day',
+        'notes',
+        'start_time',
+        'end_time',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
